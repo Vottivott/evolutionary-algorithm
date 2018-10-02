5332028def809184b60837e54d26deb70910b8c7
Hannes von Essen <hannes.von.essen@gmail.com>
2018-10-02 22:44:13 +0200

    Add main computer as argument

--- watch_fish_soccer.py
+++ watch_fish_soccer.py
@@ -1,9 +1,16 @@
 import sys
 if len(sys.argv) == 2:
     drivevariant = str(sys.argv[1])
+    maincomp = False
+    del sys.argv[1]
+elif len(sys.argv) == 3:
+    drivevariant = str(sys.argv[1])
+    maincomp=True
+    print "MAIN"
+    del sys.argv[2]
     del sys.argv[1]
 else:
     drivevariant = None
 
 from src.evoant.worm_simulation import *
-main(drivevariant)
+main(drivevariant, maincomp)
